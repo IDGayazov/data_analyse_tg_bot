1e0a748729e07103e14faaf6cdc19ade4fb2ea60
ilnaz <i.gayazov@astondevs.ru>
2024-12-20 13:36:15 +0300

    fix main file

--- main.py
+++ main.py
@@ -29,5 +29,5 @@ async def main():
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
 
-
-asyncio.run(main())
+if __name__ == "__main__":
+    asyncio.run(main())
